04893007b36d0eca4940dcf0c6adbb30d9eb47f6
MarianoPd <marianopidone.com>
2021-10-22 16:08:57 +0200

    finito

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 /** show / hide */
 
+
+/**prendo gli elementi che mi servono */
 const smblHamburger = document.querySelector('.fa-bars');
 const hamMenu = document.querySelector('.hamburger-menu');
-smblHamburger.addEventListener('click',function(){
-    
+const smblClose = document.querySelector('.close');
 
+/**se clicko sul simbolo dell'hamburger menu' mostro il menu'  */
+smblHamburger.addEventListener('click',function(){
     hamMenu.classList.add('active');
 });
 
-const smblClose = document.querySelector('.close');
-
+/**se clicko sul simbolo per chiudere il menu' nascondo il menu'  */
 smblClose.addEventListener('click',function(){
-    
     hamMenu.classList.remove('active');
 });
